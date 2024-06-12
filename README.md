56fa2ca9fec8634443e6e2a020c7532013ba2d7c
Yussuf Sassi <yussuf.sassi@krankikom.biz>
2024-06-12 17:01:21 +0200

    add missing files

---
+++ README.md
@@ -0,0 +1,20 @@
+### Datenstruktur
+
+```
+{
+    id: int;
+    date: date;
+    status: 0 | 1 | 2;
+    description: str;
+    priority: 0 | 1 | 2;
+    affected_users: int;
+}
+```
+
+### App starten
+
+dependencies:
+
+- flask
+
+`python3 web.py`
---
+++ web.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+from CSVReader import CSVReader
+from Sorter import Sorter
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    csvreader = CSVReader("data.csv")
+    data = csvreader.get_data()
+    return render_template("index.html", data=data)
+
+
+@app.route("/sort")
+def sort():
+    csvreader = CSVReader("data.csv")
+    data = csvreader.get_data()
+    sorter = Sorter()
+
+    direction = request.args.get("dir")
+
+    data = sorter.quicksort(data, "date")
+    data = sorter.quicksort(data, "priority")
+
+    if direction == "desc":
+        return render_template("index.html", data=data)
+    else:
+        return render_template("index.html", data=reversed(data))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
